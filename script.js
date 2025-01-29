ca9b0d8b00a2dabfccd7dd3c7e3d4f932685cca3
sunil meena <sunilmeena2410@gmail.com>
2025-01-29 17:53:32 +0530

    dot project

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const showResult = document.querySelector("#result");
 const undodot = [];
 const redodot = [];
 
+
 let colors = ["#DFFF00", "#FFBF00", "#FF7F50", "#DE3163", "#9FE2BF", "#40E0D0", "#6495ED", "#CCCCFF"];
 
 function getRandomColor() {
